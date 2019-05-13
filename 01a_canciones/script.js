7e75a94abd46659b73541fd77ba2fbbcb249d707
pabloHoc <hoc.pablo@gmail.com>
2019-05-13 10:59:03 -0300

    Actualizo ejercicios

---
+++ 01a_canciones/script.js
@@ -0,0 +1,20 @@
+/**
+ * Hacer un programa que pida por el nombre de la playlista, y luego actualice
+ * el elemento con id "playlist" con dicho valor.
+ * Luego, debe preguntar cuantas canciones se desee agregar
+ * y que vaya pidiendo una por una, preguntando por el nombre de la banda y la cancion. 
+ * A medida que se ingresa cada cancion, debe ir agregando al elemento html con id 
+ * "songs-container" el siguiente html:
+ * 
+ * `<div class="card mb-3">
+ *     <div class="card-header">
+ *         Nombre Banda
+ *     </div>
+ *     <div class="card-body">
+ *         <p class="card-text">Nombre cancion</p>
+ *     </div>
+ * </div>`
+ * 
+ *  Donde "Nombre banda" y "Nombre cancion" se reemplaza por el numero
+ *  por los valores ingresados, correspondientemente
+ */
