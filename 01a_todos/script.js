f765fce4a4c795b48449791586890e4131dfa9b7
pabloHoc <hoc.pablo@gmail.com>
2019-05-13 10:31:42 -0300

    Agrego archivos

---
+++ 01a_todos/script.js
@@ -0,0 +1,18 @@
+/**
+ * Hacer un programa que pida cuantas tareas se desee agregar
+ * y luego vaya pidiendo una por una. A medida que se ingresa
+ * cada tarea, debe ir agregando al elemento html con id 
+ * "todos-container" el siguiente html:
+ * 
+ * `<div class="card mb-3">
+ *     <div class="card-header">
+ *         Tarea 1
+ *     </div>
+ *     <div class="card-body">
+ *         <p class="card-text">Comprar comida</p>
+ *     </div>
+ * </div>`
+ * 
+ *  Donde "Tarea 1" y "Comprar Comida" se reemplaza por el numero
+ *  de la tarea y la tarea ingresada, correspondientemente
+ */
