f4a5fc492de9a0f1b7c10bd9077c318138a1077e
pabloHoc <hoc.pablo@gmail.com>
2019-05-13 09:03:37 -0300

    Subo archivos

--- 01_innerText/script.js
+++ 01_innerText/script.js
@@ -1,12 +1,10 @@
-let list = "";
-let item;
-
-while (item !== "terminar") {
-    item = prompt("Ingrese un item a la lista");
-    if (item !== "terminar") {
-        list += item + "\n"; 
-    }
-}
-
-document.getElementById("title").innerText = list;
-
+/**
+ * - Declarar la funcion cambiarTexto, que debe
+ * pedir en un prompt por un texto a agregar,
+ * y modificar el elemento con id "title" con
+ * el texto del prompt
+ * - Declarar la funcion borrarTexto, que debe
+ * modificar el elemento con id "title", poniendole
+ * un string vacio como html
+ * - Bindear los botones con las funciones correspondientes
+ */
