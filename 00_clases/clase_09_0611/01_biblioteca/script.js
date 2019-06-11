10610a12895edcf0757862223bb4032717052686
sasabrina <sabrinaealvarez@gmail.com>
2019-06-11 13:05:37 -0300

    ejercicio boblioteca sin terminar

---
+++ 00_clases/clase_09_0611/01_biblioteca/script.js
@@ -0,0 +1,36 @@
+/**
+ *  Crear una variable libroSeleccionado, y hacer que cuando se haga
+ * click en uno de los libros seleccionables, se guarde en dicha variable el
+ * libro que contiene
+ * Hacer que cuando se haga click en uno de los elementos con clase "estante", se agregue
+ * al array correspondiente el libro guardado en la variable libroSeleccionado,
+ * hasta un maximo de 5 libros por estante
+ * Hacer una funcion actualizarBiblioteca, que recorra el array biblioteca
+ * y vaya agregando a los nodos de clase "estante" un elemento div de clase "libro"
+ * con los items del array
+ */
+
+ /*
+ Crear una variable biblioteca, que sea un array que contenga
+ tres arrays
+ */
+const biblioteca = [[], [], []]
+
+/*
+Hacer que cuando se haga click en uno de los elementos "seleccion"
+a ese elemento se le agregue la clase "seleccionado", y a los otros
+elementos que tengan esa clase que se la saque
+*/
+const librosSeleccionables = document.getElementsByClassName('libro-seleccionable');
+
+for(let libro of librosSeleccionables){
+    libro.onclick = function(){
+        removerSeleccionados();
+        libro.classList.add('seleccionado');
+    }
+}
+const removerSeleccionados = function(){
+    for(let seleccionado of librosSeleccionables) {
+        seleccionado.classList.remove('seleccionado');
+    }
+}
