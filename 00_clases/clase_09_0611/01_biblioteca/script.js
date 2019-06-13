905a508e88ac2a713f36115b71194fd7fc0c6237
sasabrina <sabrinaealvarez@gmail.com>
2019-06-13 11:21:11 -0300

    ejercicio biblioteca terminado

--- 00_clases/clase_09_0611/01_biblioteca/script.js
+++ 00_clases/clase_09_0611/01_biblioteca/script.js
@@ -1,20 +1,9 @@
-/**
- *  Crear una variable libroSeleccionado, y hacer que cuando se haga
- * click en uno de los libros seleccionables, se guarde en dicha variable el
- * libro que contiene
- * Hacer que cuando se haga click en uno de los elementos con clase "estante", se agregue
- * al array correspondiente el libro guardado en la variable libroSeleccionado,
- * hasta un maximo de 5 libros por estante
- * Hacer una funcion actualizarBiblioteca, que recorra el array biblioteca
- * y vaya agregando a los nodos de clase "estante" un elemento div de clase "libro"
- * con los items del array
- */
-
  /*
  Crear una variable biblioteca, que sea un array que contenga
  tres arrays
  */
 const biblioteca = [[], [], []]
+let libroSeleccionado;
 
 /*
 Hacer que cuando se haga click en uno de los elementos "seleccion"
@@ -25,12 +14,50 @@ const librosSeleccionables = document.getElementsByClassName('libro-seleccionabl
 
 for(let libro of librosSeleccionables){
     libro.onclick = function(){
-        removerSeleccionados();
+        for(let seleccionado of librosSeleccionables) {
+            seleccionado.classList.remove('seleccionado');
+        }
         libro.classList.add('seleccionado');
+        /*
+        Crear una variable libroSeleccionado (línea 6), y hacer que cuando se haga
+        click en uno de los libros seleccionables, se guarde en dicha variable el
+        libro que contiene
+        */
+        libroSeleccionado = libro.innerHTML;
     }
 }
-const removerSeleccionados = function(){
-    for(let seleccionado of librosSeleccionables) {
-        seleccionado.classList.remove('seleccionado');
+
+/*
+Hacer que cuando se haga click en uno de los elementos con clase "estante", se agregue
+al array correspondiente el libro guardado en la variable libroSeleccionado,
+hasta un maximo de 5 libros por estante
+*/
+const estantes= document.getElementsByClassName('estante')
+
+for(let i = 0; i < estantes.length; i++){
+    estantes[i].onclick = function(){
+        if(biblioteca[i].length < 5){
+            biblioteca[i].push(libroSeleccionado)
+
+            actualizarBiblioteca()
+        }
+    }
+}
+
+/*
+Hacer una funcion actualizarBiblioteca, que recorra el array biblioteca
+y vaya agregando a los nodos de clase "estante" un elemento div de clase "libro"
+con los items del array
+*/
+const actualizarBiblioteca = function(){
+    for(let i = 0; i < estantes.length; i++){
+        estantes[i].innerHTML = "";
+
+        for(let j = 0; j < biblioteca[i].length; j++){
+            const libro = document.createElement('div')
+            libro.classList.add('libro')
+            libro.innerHTML = biblioteca[i][j]
+            estantes[i].appendChild(libro)
+        }
     }
 }
